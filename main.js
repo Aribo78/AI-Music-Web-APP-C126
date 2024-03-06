00be24b1b8c70dc15a8bad259b09c3ac395cc94b
Aribo78 <89050884+Aribo78@users.noreply.github.com>
2024-03-05 19:01:17 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 sound1 = ""
 sound2 = ""
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
 
 function preload(){
 sound1 = loadSound("music.mp3")
@@ -10,8 +14,16 @@ sound2 = loadSound("music2.mp3")
 function setup(){
 canvas = createCanvas(600, 500)
 canvas.center()
+
 video = createCapture(VIDEO)
 video.hide()
+
+poseNet = ml5.poseNet(video, modelLoaded)
+poseNet.on('pose', gotPoses)
+}
+
+function modelLoaded() {
+    console.log('PoseNet is Initialized')
 }
 function draw(){
 image(video, 0, 0, 600, 500)
@@ -20,3 +32,13 @@ function play(){
     sound1.play()
     sound2.play()   
 }
+function gotPoses(results) {
+    if(results.length > 0)
+    {
+        console.log(results)
+        leftWristX = results[0].pose.leftWrist.x
+        leftWristY = results[0].pose.leftWrist.y
+        rightWristX = results[0].pose.rightWrist.x
+        rightWristY = results[0].pose.rightWrist.y
+    }
+    }
