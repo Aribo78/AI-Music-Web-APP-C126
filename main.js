294be51b90cd9cdd3b110c469475bf810fe43498
Aribo78 <89050884+Aribo78@users.noreply.github.com>
2024-03-13 17:08:14 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
-sound1 = ""
-sound2 = ""
-leftWristX = 0;
-leftWristY = 0;
-rightWristX = 0;
-rightWristY = 0;
+scoreleftWrist = 0; 
+sound1 = "music.mp3"
+sound2 = "music2.mp3"
+leftWrist_x = 0;
+leftWrist_y = 0;
+rightWrist_x = 0;
+rightWrist_y = 0;
+song_name = "";
 
 function preload(){
 sound1 = loadSound("music.mp3")
@@ -27,6 +29,23 @@ function modelLoaded() {
 }
 function draw(){
 image(video, 0, 0, 600, 500)
+fill("red")
+stroke("red")
+
+song_name = sound1.isPlaying();
+console.log(Song1);
+
+if(scoreleftWristX > 0.2){
+circle(leftWrist_x, leftWrist_y, 20)
+sound2.stop();
+if(song_name == false){
+    sound1.play();
+}
+else{
+    console.log("Song Name: Track 1");
+    document.getElementById("song_id").innerHTML = "Song Name: Track 1";
+}
+}
 }
 function play(){
     sound1.play()
@@ -35,10 +54,17 @@ function play(){
 function gotPoses(results) {
     if(results.length > 0)
     {
-        console.log(results)
-        leftWristX = results[0].pose.leftWrist.x
-        leftWristY = results[0].pose.leftWrist.y
-        rightWristX = results[0].pose.rightWrist.x
-        rightWristY = results[0].pose.rightWrist.y
+        console.log(results);
+
+    scoreleftWrist = results[0].pose.keypoints[9].score;
+    console.log(scoreleftWrist);
+
+        leftWrist_x = results[0].pose.leftWrist.x;
+        leftWrist_x = results[0].pose.leftWrist.y;
+        console.log("leftWrist_x = "+leftWrist_x+" leftWrist_y = "+leftWrist_y);
+
+        rightWrist_x = results[0].pose.rightWrist.x;
+        rightWrist_y = results[0].pose.rightWrist.y;
+        console.log("rightWrist_x = "+rightWrist_x+" rightWrist_y = "+rightWrist_y);
     }
     }
